afe572d9353970be26d63c9fc25c9a725d0236c0
Louise <louisepeiffer@hotmail.com>
2021-02-09 15:50:40 +0100

    navbar js, étiquettes, etc........

--- public/js/main.js
+++ public/js/main.js
@@ -1,4 +1,5 @@
 // HEADER
+// CONNEXION
 let btnConnexion = document.querySelector('.btnConnexion')
 let modalbg = document.querySelector('.modal-bg')
 let closeModal = document.querySelector('.closeModal')
@@ -12,6 +13,36 @@ closeModal.addEventListener('click', function(){
     modalbg.classList.remove('bg-active')
 })
 
+// console.log(modalContenu.children)
+// modalbg.addEventListener('click', (e)=> {
+//     let item = e.target
+//     if (item == modalContenu && item == modalContenu.children) {
+//     } else {
+//         modalbg.classList.remove('bg-active')
+//     }
+// })
+// INSCRIPTION 
+let btnConnexion2 = document.querySelector('.btnConnexion2')
+let btnInscription = document.querySelector('.btnInscription')
+let cbFemme = document.querySelector('.checkBoxFemme')
+let cbHomme = document.querySelector('.checkBoxHomme')
+let lab1supp = document.querySelector("#lab1supp")
+let inp1supp = document.querySelector('#inp1supp')
+
+btnInscription.addEventListener('click', () => {
+    lab1supp.style.display = 'block'
+    inp1supp.style.display = 'block'
+})
+
+btnConnexion2.addEventListener('click', ()=> {
+    lab1supp.style.display = 'none'
+    inp1supp.style.display = 'none'
+})
+
+// btnConnexion.addEventListener('click')
+
+
+
 // modalbg.addEventListener('click', function(e){
 //     let item = e.target
 //     if (item == modalContenu) {
@@ -20,3 +51,28 @@ closeModal.addEventListener('click', function(){
 //     modalbg.classList.remove('bg-active')
 //     }
 // })
+
+// SCROLL NAVBAR 
+
+// Fonction ONSCROLL
+window.onscroll = function () {myFunction()}
+
+// Navbar et titre
+let navbar = document.querySelector(".divnav")
+let titreNav = document.querySelector(".logonav")
+
+// Position NavBar
+let stickyNav = navbar.offsetTop
+
+// Ajouter/Retirer classes
+function myFunction() {
+    if (window.pageYOffset >= stickyNav) {
+        navbar.classList.add("sticky")
+        titreNav.classList.add("stickyLogo")
+    } else {
+        navbar.classList.remove("sticky")
+        titreNav.classList.remove("stickyLogo")
+    }
+}
+
+
